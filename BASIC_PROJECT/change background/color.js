e2860255a0d0f1c992afb0cf1ab6384794664f00
Jatin shukla <jatinshuklaop1234@gmail.com>
2025-01-26 13:13:11 +0530

    Add files via upload

---
+++ BASIC_PROJECT/change background/color.js
@@ -0,0 +1,29 @@
+const randomColor=function(){
+    const hex="0123456789ABCDEF"
+    let color='#'
+    for(i=0; i<6; i++){
+      color+=hex[Math.floor(Math.random() * 16 )]
+  
+    }
+    return color;
+  }
+  let intervalId;
+  const startChangeColor = function()
+  {
+    if (!intervalId){
+    intervalId=setInterval(chnagebgcolor,1000);
+    }
+    function chnagebgcolor(){
+      document.body.style.backgroundColor=randomColor();
+  
+    }
+  }
+  
+  
+  const stopChangeColor=function(){
+    clearInterval(intervalId)
+    intervalId=null;
+  }
+   
+  document.querySelector('#start').addEventListener('click',startChangeColor);
+  document.querySelector('#stop').addEventListener('click',stopChangeColor);
